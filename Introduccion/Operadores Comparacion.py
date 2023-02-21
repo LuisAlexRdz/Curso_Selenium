5bbe8f36899deff8e6871b418c2ecac982113419
LuisAlexRdz <rdz.alex83@gmail.com>
2023-02-21 11:19:31 -0600

    21/02/2023

---
+++ Introduccion/Operadores Comparacion.py
@@ -0,0 +1,13 @@
+#declara variables
+a=10
+b=6
+c=11
+
+#operadores matematicos y relacionales
+print("Son iguales: "+ str(a==b))
+print("Es a menor que b: "+ str(a<=b))
+print("Es a mayor que b: "+ str(a>=b))
+print("Son diferentes: "+ str(a!=b))
+
+#Operadores de logicos
+print("sia e menor que b y a es menor que c: "+str(a<b and a<c))
