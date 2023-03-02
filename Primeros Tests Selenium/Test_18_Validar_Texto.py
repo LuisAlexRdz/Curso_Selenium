264f793d4b7c3d4127b4d04fd3336ed05cdb54bc
LuisAlexRdz <rdx.alex83@gmail.com>
2023-03-02 17:12:38 -0600

    02/03/2023

--- Primeros Tests Selenium/Test_18_Validar_Texto.py
+++ Primeros Tests Selenium/Test_18_Validar_Texto.py
@@ -18,19 +18,34 @@ driver.maximize_window()
 #espera el tiempo definido para esperar a que localice los objetos
 driver.implicitly_wait(10)
 
-btn = driver.find_element(By.XPATH, "//button[contains(@data-testid,'signup-button')]").click()
+btn = driver.find_element(By.XPATH, "//button[contains(@data-testid,'signup-button')]")
+btn.click()
 full_name = driver.find_element(By.XPATH, "//p[@data-testid='errors-name'][contains(.,'Please enter your name')]")
 name = full_name.text
-
-print("El valor del error es: " + name)
+#print("El valor del error es: " + name)
 
 if (name=="Please enter your name"):
     #print("Falta el nombre")
     cn = driver.find_element(By.XPATH, "//input[contains(@data-testid,'name')]")
     cn.send_keys("Alex Rdz")
     time.sleep(t)
+
 else:
     print("Nombre correcto")
+
+ap_name = driver.find_element(By.XPATH, "//p[contains(@data-testid,'errors-email')]")
+ap = ap_name.text
+if (ap == "Please enter your email address"):
+    # print("Falta el nombre")
+    ap = driver.find_element(By.XPATH, "//input[contains(@id,'email')]")
+    ap.send_keys("alex@gmail.com")
+    time.sleep(t)
+else:
+    print("Apellido correcto")
+
+print(btn.is_enabled())
+if (btn.is_enabled()==False):
+    print("Faltan campos por llenar")
 
 time.sleep(t)
 driver.close()
