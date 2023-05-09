1f6bb941d75e41d794b4ea92b7af925c83f6989d
LuisAlexRdz <rdx.alex83@gmail.com>
2023-05-09 17:04:36 -0600

    09/05/2023

--- PYTEST/Fixture_Decorate_2.py
+++ PYTEST/Fixture_Decorate_2.py
@@ -1,9 +1,16 @@
+###Ruta server allure: http://192.168.137.1:50881/index.html###
+###Ejecutar test: pytest .\Fixture_Decorate_2.py --alluredir="./allurereports"###
+###abrir reporte: allure serve .\allurereports\
+
 import time
+
+import allure
 import pytest
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from  allure_commons.types import AttachmentType
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,6 +23,13 @@ from  selenium.webdriver import ActionChains
 t= 1
 f=""
 driver=""
+
+@pytest.fixture()
+def log_on_failure(request):
+    yield
+    item = request.node
+    if item.rep_call.failed:
+        allure.attach(driver.get_screenshot_as_png(), name="Error", attachment_type=AttachmentType.PNG)
 
 @pytest.fixture(scope="module")
 def setup_login_uno():
@@ -42,15 +56,20 @@ def setup_login_dos():
     driver.get("https://test.igniite.io/")
     ##driver.maximize_window()
     f = Funciones_Globales(driver)
+    allure.attach(driver.get_screenshot_as_png(), name="oraxio", attachment_type=AttachmentType.PNG)
     f.Click_Mixto("xpath","(//button[contains(.,'GET STARTED')])[2]",t)
+    allure.attach(driver.get_screenshot_as_png(), name="login", attachment_type=AttachmentType.PNG)
     f.Texto_Mixto("id", "user", "LuisAlexRdz", t)
+    allure.attach(driver.get_screenshot_as_png(), name="user", attachment_type=AttachmentType.PNG)
     f.Texto_Mixto("id", "password", "Alex1983", t)
+    allure.attach(driver.get_screenshot_as_png(), name="psw", attachment_type=AttachmentType.PNG)
     f.Click_Mixto("xpath", "//button[@type='submit'][contains(.,'LOGIN')]", 3)
     print("Entrando al sistema")
     ##yield
     ##print("Saliendo del login dos")
     ##driver.close()
 
+@pytest.mark.usefixtures("log_on_failure")
 @pytest.mark.usefixtures("setup_login_uno")
 def test_uno():
     print("Entrando al sistema uno")
@@ -76,14 +95,19 @@ def test_uno():
     f.Click_Mixto("xpath","//textarea[@id='AdminComment']",3)
 
 
-
-
-
+@pytest.mark.usefixtures("log_on_failure")
 @pytest.mark.usefixtures("setup_login_dos")
 def test_dos():
     print("Entrando al sistema dos")
+    f.Click_Mixto("xpath","//span[contains(.,'LIVE TRADING')]",t)
+    allure.attach(driver.get_screenshot_as_png(),name="LIVE_TRADING",attachment_type=AttachmentType.PNG)
+    f.Click_Mixto("xpath","//span[contains(.,'ACCOUNTS')]",t)
+    allure.attach(driver.get_screenshot_as_png(), name="ACCOUNTS", attachment_type=AttachmentType.PNG)
+    f.Click_Mixto("xpath","(//span[contains(.,'ADMIN')])[1]",t)
+    allure.attach(driver.get_screenshot_as_png(), name="ADMIN", attachment_type=AttachmentType.PNG)
+    f.Click_Mixto("xpath","//span[contains(.,'HOME')]",t)
+    driver.get_screenshot_as_file(f"./(home).png")
     time.sleep(5)
-
 
 
 def teardown_function():
